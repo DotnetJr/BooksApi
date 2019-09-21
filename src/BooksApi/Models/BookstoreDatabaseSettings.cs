62dd5915d94078f7cd473f748dba40b373a326de
Ivan <tabarino@outlook.com>
2019-09-21 19:18:16 +0100

    Initial Commit

---
+++ src/BooksApi/Models/BookstoreDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BooksApi.Models
+{
+    public class BookstoreDatabaseSettings : IBookstoreDatabaseSettings
+    {
+        public string BooksCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ src/BooksApi/Models/IBookstoreDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BooksApi.Models
+{
+    public interface IBookstoreDatabaseSettings
+    {
+        string BooksCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
